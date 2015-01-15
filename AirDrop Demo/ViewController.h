261816c5e3fb7ac795f40417ab547b0e3bd58480
Ramakrishna Makkena <ramakrishnamakkena@ramakrishnasmbp.gateway.pace.com>
2015-01-15 15:32:11 -0500

    Initial Commit

---
+++ AirDrop Demo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AirDrop Demo
+//
+//  Created by Ramakrishna Makkena on 1/15/15.
+//  Copyright (c) 2015 nwmissouri. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ AirDrop DemoTests/AirDrop_DemoTests.m
@@ -0,0 +1,40 @@
+//
+//  AirDrop_DemoTests.m
+//  AirDrop DemoTests
+//
+//  Created by Ramakrishna Makkena on 1/15/15.
+//  Copyright (c) 2015 nwmissouri. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface AirDrop_DemoTests : XCTestCase
+
+@end
+
+@implementation AirDrop_DemoTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
